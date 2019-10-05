0ad6f083294dcfe6d8475ed73952d1aa92f13350
Anton Kulakov <kulakowka@gmail.com>
2019-10-06 01:01:31 +0300

    clean up

--- apps/video/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb://localhost:27017/microservice-app",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-
-module.exports = mongoose.model("User", { name: String });
